94ff0466360deff8143ed149e545080c864505aa
Ian Waedemon <arcanecenotaph@gmail.com>
2022-03-19 17:35:42 -0500

    Add 03-10-22 coding challenge.

--- 8kyu kata/Find Maximum and Minimum Value of a List.py
+++ 8kyu kata/Find Maximum and Minimum Value of a List.py
@@ -5,6 +5,7 @@ Examples (Input -> Output)
 * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
 * [42, 54, 65, 87, 0]             -> min = 0, max = 87
 * [5]                             -> min = 5, max = 5
+
 Notes
 You may consider that there will not be any empty arrays/vectors. """
 
