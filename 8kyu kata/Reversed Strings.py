05d94bebe976d99bc6548c999e04aed800e8a0dd
Ian Waedemon <arcanecenotaph@gmail.com>
2022-03-25 18:00:55 -0500

    Add 03-25-22 coding challenge.

--- 8kyu kata/Reversed Strings.py
+++ 8kyu kata/Reversed Strings.py
@@ -1,7 +1,8 @@
 """ Complete the solution so that it reverses the string passed into it.
 
 'world'  =>  'dlrow'
-'word'   =>  'drow' """
+'word'   =>  'drow'
+'red'    =>  'der' """
 
 def solution(string):
     return string[::-1]
