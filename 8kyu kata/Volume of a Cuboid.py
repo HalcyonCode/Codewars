802f35c7173b122e3b88ef3a018546c5c28b25c3
Ian Waedemon <arcanecenotaph@gmail.com>
2022-03-28 18:06:25 -0500

    Add 03-28-22 coding challenge.

--- 8kyu kata/Volume of a Cuboid.py
+++ 8kyu kata/Volume of a Cuboid.py
@@ -1,4 +1,6 @@
-""" Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation. """
+""" Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. 
+
+Write a function to help Bob with this calculation. """
 
 def getVolumeOfCubiod(length, width, height):
     volume = length * width * height
